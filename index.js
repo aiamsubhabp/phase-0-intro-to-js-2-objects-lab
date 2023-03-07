54437ce92c276cdfd9be7b377caca4d5a34d36de
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-03-06 23:07:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Write your solution in this file!
+/*// Write your solution in this file!
 const employee = {
 //name: "Kevin",
 //streetAddress: "Maple Ave"
@@ -23,4 +23,36 @@ function deleteFromEmployeeByKey(employee, key){
 function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key];
     return employee;
+}
+*/
+
+
+const employee = {
+    name: 'Paul',
+    streetAddress: 'rockville pike' 
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee, 
+        [key]:value
+    }
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let employeeUpdate = {...employee};
+    delete employeeUpdate[key];
+    return employeeUpdate;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+
 }
